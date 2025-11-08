51eacd8d310bc5410222aef9216c0756d11ffa4b
Hugo Ghesquier <hugo.ghesquier59@gmail.com>
2025-11-08 16:35:27 +0100

    feat: add Struct contract for managing user balances and payments

---
+++ contracts/Struct.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+contract Struct {
+
+    struct balance {
+        uint money;
+        uint numPayments;
+    }
+
+    mapping(address => balance) Balances;
+
+    function getBalance() public view returns(uint) {
+        return Balances[msg.sender].money;
+    }
+
+    function getNumPayments() public view returns(uint) {
+        return Balances[msg.sender].numPayments;
+    }
+
+    receive() external payable {
+        Balances[msg.sender].money += msg.value;
+        Balances[msg.sender].numPayments += 1;
+    }
+}
